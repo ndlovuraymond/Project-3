1f6985bd26667cf16e299801b25855979bc7ff85
Raymond Ndlovu <93596122+ndlovuraymond@users.noreply.github.com>
2023-02-17 01:21:40 +0200

    Add files via upload

--- Methods.py
+++ Methods.py
@@ -1,91 +1,97 @@
-class Polynomial_Calc:
-    def __init__(self):
-        self.points = []
-        self.coefficients = []
+import re
+import numpy as np
 
-    def display_values(self):
-        print("Values of the polynomial:")
-        for x in [-1, 0, 1]:
-            y = 0
-            for i in range(len(self.coefficients) - 1, -1, -1):
-                y = y * x + self.coefficients[i]
-            print(f"f({x}) = {y}")
+# giving the user instructions
+print("Please input points in x y representation. \nType END to finish.")
 
-    def show_values(self):
-        print("Please input points in x y representation. \nType END to finish.")
-        self.input_points()
-        self.display_values()
-        self.find_root(0, 1, 1e-6)
-        print(f"The root found for x = {self.find_root(0, 1, 1e-6)}")
-
-    def input_points(self):
-        self.points = []
-        inputting_data = True
-        i = 1
-        # collecting coordinates
-        while inputting_data:
-            value = input(f"Enter the x and y coordinates of point {i}: ")
-            if len(value.split()) == 2:
-                cur_value = value.split()
-                x = float(cur_value[0])
-                y = float(cur_value[1])
-                self.points.append((x, y))
-                i += 1
-            # ending the collection based on user input
-            elif value.upper() == "END":
-                inputting_data = False
-            else:
-                print("Please enter coordinates like this: 2 1 where X is 2 and Y is 1")
-        x_axis = [p[0] for p in self.points]
-        y_axis = [p[1] for p in self.points]
-        n = len(x_axis)
-        vandermonde = [[xi**j for j in range(n)] for xi in x_axis]
-        coefficients = [0] * n
-
-        i = 0
-        while i < n:
-            current_pivot = vandermonde[i][i]
-            if current_pivot == 0:
-                i += 1
-                continue
-            p = i + 1
-            while p < n:
-                factor = vandermonde[p][i] / current_pivot
-                for k in range(n):
-                    vandermonde[p][k] -= factor * vandermonde[i][k]
-                y_axis[p] -= factor * y_axis[i]
-                p += 1
-            i += 1
-
-        count = n - 1
-        while count > -1:
-            cur_pivot = vandermonde[count][count]
-            if cur_pivot == 0:
-                coefficients[count] = 0
-            else:
-                coefficients[count] = y_axis[count] / vandermonde[count][count]
-                for j in range(i):
-                    y_axis[j] -= vandermonde[j][count] * coefficients[count]
-            count += -1
-
-        self.coefficients = coefficients
-
-    def find_root(self, x0, x1, eps):
-        f0 = 0
-        f1 = 0
-        for i in range(len(self.coefficients) - 1, -1, -1):
-            f0 = f0 * x0 + self.coefficients[i]
-            f1 = f1 * x1 + self.coefficients[i]
-        while abs(f1) > eps:
+# accepting user inputs
+inputting_data = True
+points = []
+count = 1
+while inputting_data:
+    try:
+        user_points = input(f"P#{count}: ")
+        if user_points.upper() == "END":
+            break
+        else:
             try:
-                x2 = x1 - f1 * (x1 - x0) / (f1 - f0)
-                f2 = 0
-                for i in range(len(self.coefficients) - 1, -1, -1):
-                    f2 = f2 * x2 + self.coefficients[i]
-                x1 = x2
-                f1 = f2
-                root = x1
-            # try except block incase zero division
-            except ZeroDivisionError:
-                return 0
-        return root
+                cur_points = user_points.split()
+                x, y = cur_points
+                x = float(x)
+                y = float(y)
+                current_points = [x, y]
+                points.append(current_points)
+            except:
+                print("Please enter valid points e.g. 1 2 where x=1 & y=2")
+                continue
+    except:
+        print("Please enter valid points e.g. 1 2 where x=1 & y=2")
+        continue
+    count += 1
+
+# displaying the function in the form f(x)
+x = [x[0] for x in points]
+y = [y[1] for y in points]
+function = ""
+# finding the values for the function f(x)
+values = np.polyfit(x, y, 2)
+a, b, c = values
+a = round(a, 4)
+b = round(b, 5)
+c = round(c, 5)
+print("Resulting polynomial will be of the order 2")
+count = 1
+string = "f(x) = "
+for item in values:
+    if count == 1:
+        string += str(a) + "x^2"
+    elif count == 2:
+        if b >= 0:
+            string += " + "
+        else:
+            b = b * -1
+            string += " - "
+        string += str(b) + "x^1"
+    elif count == 3:
+        if c >= 0:
+            string += " + "
+        else:
+            c = c * -1
+            string += " - "
+        string += str(c)
+    count += 1
+print(string)
+# making the function into the string so that I can use it to get the derivative later
+function = string
+# display polynomial for -1,0,1
+a, b, c = values
+a = float(a)
+b = float(b)
+c = float(c)
+for i in range(-1, 2):
+    result = 0
+    for j in range(0, 3):
+        result = result * i + values[j]
+    string = f"f({i}) = " + str(result)
+    print(string)
+
+# getting the derivative of the function
+def poly_dev(poly, dev):
+    c = 0
+    string = ""
+    for i in range(1, 3):
+        poly[i - 1] = (len(poly) - (i + c)) * poly[i - 1]
+        cur_string = str(round(poly[i - 1], 9))
+        if i == 1:
+            string += cur_string
+            string += "x^1"
+        else:
+            string += cur_string
+    c += 1
+    return string
+
+
+# showing the user the derivative
+print("Derivative:")
+derivative = poly_dev(values, 1)
+print("f'(x) =" + str(derivative))
